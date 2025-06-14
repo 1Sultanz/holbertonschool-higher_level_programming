f81ebfcda721aabbac176512562882c85669bcc9
1Sultanz <z.r.amin40@gmail.com>
2025-06-14 10:19:32 +0000

    task 6

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 i = 0
 while i <= 99:
-    print("{:02}".format(i), end=", ")
-    i += 1
+    if i < 99:
+        print("{:02}".format(i), end=", ")
+    else:
+        print("{:02}".format(i))
+    i += 1        
