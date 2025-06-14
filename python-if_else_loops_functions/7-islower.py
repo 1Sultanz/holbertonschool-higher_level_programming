11f3a24af4b97795b9d831e1fe42fb8723aeb858
1Sultanz <z.r.amin40@gmail.com>
2025-06-14 11:08:20 +0000

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def isLower(c):
-    return ord(c) >= ord("a") and ord(c) <= ord("z") 
+    return ord(c) >= ord("a") and ord(c) <= ord("z")
+    print("a is {}".format("lower" if islower("a") else "upper"))
