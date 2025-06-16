88af088ca9e1aff5464604f5f79c33073f7929ba
1Sultanz <z.r.amin40@gmail.com>
2025-06-16 16:09:03 +0000

    task 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def arguments(argv):
-    while i < len(argv)
-    i = 0 
-    print("1: {}".format(argv[i]))
-    i += 1
+    while i < len(argv):
+    i = 1
+        print("{}: {}".format(i, argv[i]))
+        i += 1
