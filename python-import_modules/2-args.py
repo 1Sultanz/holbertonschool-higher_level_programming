568b7b7e1ad134110cd67377503cf50f8104df63
1Sultanz <z.r.amin40@gmail.com>
2025-06-16 16:31:42 +0000

    Task 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 if argument_count == 0:
     print("0 arguments.")
 elif argument_count == 1:
-    print("1 arguments:")
+    print("1 argument:")
 else:
     print("{} arguments:".format(argument_count))
 
