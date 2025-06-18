88c005547f3f8b1f1f74bd40c68aa0cb5e90491c
1Sultanz <z.r.amin40@gmail.com>
2025-06-18 12:55:00 +0000

    task 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -3,8 +3,8 @@
 def print_list_integer(my_list=[]):
    i = 0
    while i < len(my_list):
-        print("{}".format(my_list[i]))
-        i += 1
+       print("{:d}".format(my_list[i]))
+       i += 1
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
