b1c5a066b45b7246cb3900dde4a196c51a1e14eb
1Sultanz <z.r.amin40@gmail.com>
2025-06-18 12:42:59 +0000

    task 1

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+my_list = [1, 2, 3, 4, 5]
+
+import sys
+
+if __name__ == "__main__":
+    i = 0
+    while i < len(my_list):
+        print("{}".format(my_list[i]))
+        i += 1
+
