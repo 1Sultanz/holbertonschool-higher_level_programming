c77dca532eb7713aff208b7e0f497d6ccb3f9b69
1Sultanz <z.r.amin40@gmail.com>
2025-06-18 12:50:43 +0000

    task 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
 
 import sys
 
-if __name__ == "__main__":
-    i = 0
-    while i < len(my_list):
+def print_list_integer(my_list=[]):
+   i = 0
+   while i < len(my_list):
         print("{}".format(my_list[i]))
         i += 1
 
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
