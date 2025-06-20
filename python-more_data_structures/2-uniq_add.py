92c11b5813b807d415495761d26a52716e3ac8e6
1Sultanz <z.r.amin40@gmail.com>
2025-06-20 13:31:16 +0000

    pip

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -7,7 +7,7 @@ def uniq_add(my_list=[]):
         total += n
     return total
 
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 1, 4, 2, 5]
-    result = uniq_add(my_list)
-    print("Result: {:d}".format(result))
+    if __name__ == "__main__":
+        my_list = [1, 2, 3, 1, 4, 2, 5]
+        result = uniq_add(my_list)
+        print("Result: {:d}".format(result))
