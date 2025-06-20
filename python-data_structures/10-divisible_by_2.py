ccc5c3fdc8070211a50d9c550da457b60de96531
1Sultanz <z.r.amin40@gmail.com>
2025-06-20 07:35:25 +0000

    task 11

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    del my_list[idx]
-    return my_list
+def divisible_by_2(my_list=[]):
+    i = 0
+    result = []
+    while i < len(my_list):
+        result.append(my_list[i] % 2 == 0)
+        i += 1
+    return result
 
 if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
+    my_list = [0, 1, 2, 3, 4, 5, 6]
+    list_result = divisible_by_2(my_list)
+
+    i = 0
+    while i < len(list_result):
+        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+        i += 1
