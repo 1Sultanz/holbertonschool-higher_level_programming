ebeb13857b76d4b301fad06a7e6cf3bee0efe603
1Sultanz <z.r.amin40gmail.com>
2025-06-26 09:43:18 +0000

    task 1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write an empty class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
