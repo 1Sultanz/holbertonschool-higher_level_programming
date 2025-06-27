baacf457169f31f783447773bd5030c7cfdb35cc
1Sultanz <z.r.amin40@gmail.com>
2025-06-27 20:18:02 +0000

    task 5

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""Defines an inheritance-checking function."""
 
+
+def inherits_from(obj, a_class):
+    """
+    Return True if the object is an instance of a class
+    that inherited (directly or indirectly) from a_class.
+    Return False if the object is an instance of a_class itself.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
