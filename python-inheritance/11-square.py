6dbf8b40f27e16c9ccb69c7882f421b07b0972c2
1Sultanz <z.r.amin40@gmail.com>
2025-06-28 15:33:29 +0000

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle (based on 10-square.py)."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square shape."""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        # Call the parent Rectangle constructor with width and height equal to size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
