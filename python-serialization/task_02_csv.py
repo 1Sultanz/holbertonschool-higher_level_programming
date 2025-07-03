b48dad3fc46f028929a6d6463b6d491fc9034713
1Sultanz <z.r.amin40@gmail.com>
2025-07-03 10:06:03 +0000

    task 3

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+python import csv import json
+
+
+def convert_csv_to_json(filename):
+    try:
+        with open(filename, "r",  encoding = "utf-8") as csvfile:
+            r_file = csv.DictReader(csvfile)
+            data = list(r_file)
+
+        with open("data.json", "w", encoding = "utf-8") as jsonfile:
+            json.dump(data, jsonfile, indent = 4)
+
+    except FileNotFoundError:
+        return False
+    except:
+        return False
