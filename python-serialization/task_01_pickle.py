51963bc0bd8db6980a65b63a7d71f9ae87924238
1Sultanz <z.r.amin40@gmail.com>
2025-07-03 09:16:19 +0000

    task 2

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import pickle
+
+class CustomObject:
+    """This class is about custom"""
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name},
+            Age: {self.age},
+            Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+
+        except Exception as e:
+            print(f"Serialization Error: {e}")
+
+    @classmethod
+    def  deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                dictioanary = pickle.load(f)
+            return dictionary
+        except Exception as e:
+            print(f"Desrialization Error: {e}")`
