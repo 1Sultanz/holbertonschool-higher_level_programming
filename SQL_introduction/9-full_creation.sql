edc4314f8161915d3f50c09e992e52689e43e5be
1Sultanz <z.r.amin40@gmail.com>
2025-07-03 13:53:59 +0000

    task 10

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,13 @@
 -- Create second_table
-CREATE TABLE second_table(
-	id INT,
-	name VARCHAR(256),
-	score INT
+CREATE TABLE second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
 );
 
-INSERT INTO second_table
-VALUES(id = 1, name = John, score = 10,
-id = 2, name = Alex, score = 3,
-id = 3, name = Bob, score = 14,
-id = 4, name = Georg, score = 8);
+-- Insert records correctly
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
