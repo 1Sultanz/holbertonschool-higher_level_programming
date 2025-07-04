f652eeb8732972e0eecc2e8f67f96322a4e171d3
1Sultanz <z.r.amin40@gmail.com>
2025-07-04 20:29:07 +0000

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- JOIN TABLE
 SELECT cities.id, cities.name, states.name
-FROM cities FULL OUTER JOIN states ON cities.id = states.id
+FROM cities CROSS JOIN states ON cities.id = states.id
 ORDER BY cities.id;
