15b2d3d0ad9f829ff765f23c6ad67e8bb3d72502
1Sultanz <z.r.amin40@gmail.com>
2025-07-04 12:36:18 +0000

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,3 @@
--- My privileges!
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1-z';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2-z';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
-FLUSH PRIVILEGES;
+-- Grant Lists
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
