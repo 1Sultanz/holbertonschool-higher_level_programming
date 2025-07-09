b22276c6f5a98d5976584b7f7df402115ac861bd
1Sultanz <z.r.amin40@gmail.com>
2025-07-09 15:37:40 +0400

    task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Load, add, save"""
+
 import sys
+"""Load, add, save"""
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
