df02c41b621f9fadcc934f82164b5c5dda19f6f0
1Sultanz <z.r.amin40@gmail.com>
2025-07-10 22:12:05 +0400

    task

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """
+    This function retrieves a dictionary 
+    representation of a Student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    @publicmethod
+    def to_json(self):
+        return self.__dict__
