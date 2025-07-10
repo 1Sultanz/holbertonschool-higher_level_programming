1ee9c2cc1cc58cc06ef7d11c3db9fbf830d43726
1Sultanz <z.r.amin40@gmail.com>
2025-07-10 22:05:50 +0400

    task

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Class to json"""
 
+
 def class_to_json(obj):
-    """This function reutrns the dictionary with simple data structure for JSON serialization of an object"""
+    """
+    This function reutrns the dictionary with simple data structure 
+    for JSON serialization of an object
+    """
     return obj.__dict__
