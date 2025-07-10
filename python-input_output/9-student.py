849ff79ab1c8bcae1b5829e88b49f18093b4714a
1Sultanz <z.r.amin40@gmail.com>
2025-07-10 22:13:41 +0400

    task

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -11,7 +11,8 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+ 
+    """Module documented"""
     @publicmethod
     def to_json(self):
         return self.__dict__
