1f2043ef785bd5b22cdfd83ce95f043321fbdd61
1Sultanz <z.r.amin40@gmail.com>
2025-07-10 22:15:21 +0400

    task

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,14 +4,14 @@
 
 class Student:
     """
-    This function retrieves a dictionary 
+    This function retrieves a dictionary
     representation of a Student instance
     """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
- 
+
     """Module documented"""
     def to_json(self):
         return self.__dict__
