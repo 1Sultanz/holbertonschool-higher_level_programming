e619ff768acf92978757d80a8acbb243891016bf
1Sultanz <z.r.amin40@gmail.com>
2025-07-11 17:58:31 +0400

    task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,6 +7,12 @@ import os
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_list = sys.argv[1:]
-save_to_json_file(my_list, "add_item.json")
-load_from_json_file("add_item.json")
+filename = "add_item.json"
+
+try:
+    items = load_fro_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:]
+save_to_json_file(items, filename)
