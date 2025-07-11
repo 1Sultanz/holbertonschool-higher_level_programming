fa855ff70381082ba5737837bd732020dd0396a6
1Sultanz <z.r.amin40@gmail.com>
2025-07-11 20:28:20 +0400

    task

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """
+    This function retrieves a dictionary
+    representation of a Student instance
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """Module documented"""
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(attr) is str for attr in attrs):
+            return {attr: getattr(
+                self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
