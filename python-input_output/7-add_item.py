085b5492101fefe7476815a740f2268a3d50e7e5
1Sultanz <z.r.amin40@gmail.com>
2025-07-11 17:43:03 +0400

    argv

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Save, add, load"""
-import json
+import sys
+import os
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_list = [1, 2, 3, 4]
+my_list = sys.argv[1:]
 save_to_json_file(my_list, "add_item.json")
 load_from_json_file("add_item.json")
