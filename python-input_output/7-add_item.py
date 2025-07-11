080fa2ff5530c7e1ed3bf1a25049b71cda98d048
1Sultanz <z.r.amin40@gmail.com>
2025-07-11 17:18:02 +0400

    task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,17 +2,8 @@
 """Save, add, load"""
 import json
 
-
-def save_to_json_file(my_obj, filename):
-    """This function that writes an Object to a text file"""
-    with open(filename, "w", encoding='utf-8') as f:
-        a = json.dump(my_obj, f)
-        return a
-
-def load_from_json_file(filename):
-    """A function that creates an Object from a json file"""
-    with open(filename, "r", encoding='utf-8') as f:
-        return json.load(f)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_list = [1, 2, 3, 4]
 save_to_json_file(my_list, "add_item.json")
