d794fb36ede85f40ad74ca9c444f4791e4cb04b9
1Sultanz <z.r.amin40@gmail.com>
2025-07-14 14:23:09 +0400

    task

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    total = 0
+    for index, char in enumerate(roman_string):
+        if index == len(roman_string) - 1:
+            total += roman[char]
+        elif roman[char] < roman[roman_string[index+1]]:
+            total = total + (roman[roman_string[index+1]] - roman[char])
+            break
+        else:
+            total += roman[char]
+    return total
+
+print(roman_to_int("XIX"))
